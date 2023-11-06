5f102b6f138a49fdb86f22ddc48789711fcd74a9
kaid-0 <darrenlewisworks@gmail.com>
2023-11-06 16:29:43 +0530

    Added signup functionality for the user

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # Defining routes for articles
   resources :articles
+
+  # Defining routes for user signup
+  get 'signup' => 'users#new'
+  resources :users, except: [:new] # Defines all the routes for the users and it's controller, except for new. Because new is already defined for signup
+  
 end
